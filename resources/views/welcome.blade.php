83f0c11c0edb145d2c72ccdfbc390b5c08bdc32b
AlanDAmario <vega93@live.it>
2024-07-12 15:51:24 +0200

    feat: add seeder and layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Laravel Template</title>
-        @vite('resources/js/app.js')
+@extends('layouts.app')
 
-    </head>
-    <body>
-        
-    </body>
-    </html>
+@section('main')
+    <h2>sono il main</h2>
+@endsection
