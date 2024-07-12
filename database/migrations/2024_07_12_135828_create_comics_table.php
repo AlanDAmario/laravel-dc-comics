ad1d4e38dfef010177d5d21cea4d509dbff176c0
AlanDAmario <vega93@live.it>
2024-07-12 18:32:13 +0200

    feat: add migrations and seeder

--- database/migrations/2024_07_12_135828_create_comics_table.php
+++ database/migrations/2024_07_12_135828_create_comics_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use function Laravel\Prompts\table;
+
 return new class extends Migration
 {
     /**
@@ -13,6 +15,16 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            
+            $table->string('title', 255);
+            $table->text('description');
+            $table->string('thumb', 512);
+            $table->string('price', 50);
+            $table->string('series', 255);
+            $table->string('sale_date', 50);
+            $table->string('type', 100);
+            $table->json('artists'); //riga per memorizzare gli artisti come JSON
+
             $table->timestamps();
         });
     }
--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -14,10 +14,20 @@ class ComicSeeder extends Seeder
     public function run(): void
     {
         $comics = config('comics');
-        foreach ($comics as $comic) 
-        $comic = new Comic();
+        foreach ($comics as $comic_data) {
+            
+            $comic = new Comic();
 
-        $comic->title = $comic['title'];
+            $comic->title = $comic_data['title'];
+            $comic->description = $comic_data['description'];
+            $comic->thumb = $comic_data['thumb'];
+            $comic->price = $comic_data['price'];
+            $comic->series = $comic_data['series'];
+            $comic->sale_date = $comic_data['sale_date'];
+            $comic->type = $comic_data['type'];
+            $comic->artists = json_encode($comic_data['artists']); // Codifica l'array come JSON
 
-        $comic-> save();
+            $comic->save();
+        }
+    }
 }
